239efc6d806e30d38fac9a204b26f45b86054bac
marcortiz <mrk.22.10@gmail.com>
2013-09-09 17:45:16 +0200

    program done

---
+++ search_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int list[] = {23,12,‐16,24,25};
+	int maximum = contador = 0;
+	while(contador < 5){
+		maximum = (maximum < list[counter]) ? maximum = list[counter] : maximum; 	
+		contador++;
+	}
+	
+	cout << maximum;
+	return 0;
+}
